9033563da63104453c7616d43d251069a24bc939
Ednardo Gomes <ednardo@email.com>
2021-07-26 16:24:02 -0300

    Criação dos arquivos api.ts e convertDurationToTimeString para formatar os dados vindo do backend.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import { GetStaticProps } from 'next';
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { api } from '../services/api';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
 type Episode = {
   id: string,
   title: string,
-  members: string
+  members: string,
+  published_at: string
   // ...
 }
 
@@ -22,12 +27,31 @@ export default function Home(props: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await fetch('http://localhost:3333/episodes');
-  const data = await response.json();
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  });
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      plublishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR}),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url
+    }
+  });
 
   return {
     props: {
-      episodes: data,
+      episodes
     },
     revalidate: 60 * 60 * 8,
   }
